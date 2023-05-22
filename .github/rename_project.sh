a9b7bf8ad949af179212c5ac6ea23a978911538d
jeffamaxey <jeffamaxey@users.noreply.github.com>
2023-05-22 17:32:46 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="jeffamaxey"
+original_name="basemodel"
+original_urlname="basemodel"
+original_description="Awesome basemodel created by jeffamaxey"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv basemodel $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
